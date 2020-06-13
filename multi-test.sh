877dc3f0d6a1a0143892acb5072ae8392ff63c1a
Matt Bentley <mbentley@mirantis.com>
2020-06-13 08:59:20 -0400

    added output about DISABLE_SECCOMP
    Signed-off-by: Matt Bentley <mbentley@mirantis.com>

--- multi-test.sh
+++ multi-test.sh
@@ -24,7 +24,7 @@ do
 done
 
 # get averages of each stat
-echo -e "\nAverage stats from ${NUM_LOOPS} loops of ${NUM_EXECS} execs on docker-ee=$(dpkg -l docker-ee | grep ^ii | awk '{print $3}') for libseccomp2=$(dpkg -l libseccomp2 | grep ^ii | awk '{print $3}'):"
+echo -e "\nAverage stats from ${NUM_LOOPS} loops of ${NUM_EXECS} execs on docker-ee=$(dpkg -l docker-ee | grep ^ii | awk '{print $3}') for libseccomp2=$(dpkg -l libseccomp2 | grep ^ii | awk '{print $3}') with DISABLE_SECCOMP=${DISABLE_SECCOMP}:"
 echo "Avg Min: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_min.txt)"
 echo "Avg Max: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_max.txt)"
 echo "Avg Avg: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_avg.txt)"
