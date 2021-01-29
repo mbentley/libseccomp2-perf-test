2a3cc420bf1955da7b0ec929b0ec959357acbb6f
Matt Bentley <mbentley@mirantis.com>
2021-01-29 14:04:03 -0500

    Updates for CE
    Signed-off-by: Matt Bentley <mbentley@mirantis.com>

---
+++ multi-test-ce.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# set default loops to 10
+NUM_LOOPS="${1:-10}"
+
+# set the default number of execs to 40
+NUM_EXECS="${2:-40}"
+
+# set default to not disable seccomp on the test container
+DISABLE_SECCOMP="${DISABLE_SECCOMP:-false}"
+
+# set default to be more verbose
+VERBOSE="${VERBOSE:-true}"
+
+# let user know what test we are doing
+if [ "${VERBOSE}" = "true" ]
+then
+  echo "Running ${NUM_LOOPS} loops of ${NUM_EXECS} execs"
+fi
+
+# loop through the test
+for LOOP in $(seq 1 "${NUM_LOOPS}")
+do
+  if [ "${VERBOSE}" = "true" ]
+  then
+    echo -e "\nLoop ${LOOP}"
+  fi
+  STATS="$(DISABLE_SECCOMP="${DISABLE_SECCOMP}" ./docker-libseccomp-test.sh "${NUM_EXECS}" | grep -E '(Min:)|(Max:)|(Avg:)')"
+  echo "${STATS}" | grep Min >> stats_min.txt
+  echo "${STATS}" | grep Max >> stats_max.txt
+  echo "${STATS}" | grep Avg >> stats_avg.txt
+  if [ "${VERBOSE}" = "true" ]
+  then
+    echo "${STATS}"
+  fi
+done
+
+# get averages of each stat
+echo -e "\nAverage stats from ${NUM_LOOPS} loops of ${NUM_EXECS} execs on docker-ce=$(dpkg -l docker-ce | grep ^ii | awk '{print $3}') for libseccomp2=$(dpkg -l libseccomp2 | grep ^ii | awk '{print $3}') with DISABLE_SECCOMP=${DISABLE_SECCOMP}:"
+echo "Avg Min: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_min.txt)"
+echo "Avg Max: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_max.txt)"
+echo "Avg Avg: $(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_avg.txt)"
+
+# check to sce if we need to write headers to the csv output
+if [ ! -f "test_results.csv" ]
+then
+  echo "docker-ce version,libseccomp2 version,num_loops,num_execs,disable_seccomp,min,max,avg" > test_results.csv
+fi
+
+# write output to a log file
+echo "docker-ce=$(dpkg -l docker-ce | grep ^ii | awk '{print $3}'),libseccomp2=$(dpkg -l libseccomp2 | grep ^ii | awk '{print $3}'),${NUM_LOOPS},${NUM_EXECS},${DISABLE_SECCOMP},$(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_min.txt),$(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_max.txt),$(awk '{sum+=$2}END{printf "%0.2f\n",sum/NR}' stats_avg.txt)" >> test_results.csv
+
+# cleanup
+rm stats_min.txt stats_max.txt stats_avg.txt
--- multi-version-test-ce.sh
+++ multi-version-test-ce.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DOCKER_EE_VERS="17.06.0~ce-0~ubuntu 17.06.1~ce-0~ubuntu 17.06.2~ce-0~ubuntu 17.09.0~ce-0~ubuntu 17.09.1~ce-0~ubuntu 17.12.0~ce-0~ubuntu 17.12.1~ce-0~ubuntu 18.03.0~ce-0~ubuntu 18.03.1~ce-0~ubuntu 18.06.0~ce~3-0~ubuntu 18.06.1~ce~3-0~ubuntu 18.06.2~ce~3-0~ubuntu 18.06.3~ce~3-0~ubuntu 5:18.09.0~3-0~ubuntu-xenial"
+DOCKER_CE_VERS="17.06.0~ce-0~ubuntu 17.06.1~ce-0~ubuntu 17.06.2~ce-0~ubuntu 17.09.0~ce-0~ubuntu 17.09.1~ce-0~ubuntu 17.12.0~ce-0~ubuntu 17.12.1~ce-0~ubuntu 18.03.0~ce-0~ubuntu 18.03.1~ce-0~ubuntu 18.06.0~ce~3-0~ubuntu 18.06.1~ce~3-0~ubuntu 18.06.2~ce~3-0~ubuntu 18.06.3~ce~3-0~ubuntu 5:18.09.0~3-0~ubuntu-xenial"
 
 # install each version of docker
-for DOCKER_EE_VER in ${DOCKER_EE_VERS}
+for DOCKER_CE_VER in ${DOCKER_CE_VERS}
 do
   # install the specific version of docker
-  DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --allow-downgrades docker-ce="${DOCKER_EE_VER}" > /dev/null
+  DEBIAN_FRONTEND=noninteractive apt-get install -qq -y --allow-downgrades docker-ce="${DOCKER_CE_VER}" > /dev/null
 
   # run test w/seccomp disabled
-  DISABLE_SECCOMP=true VERBOSE=false ./multi-test.sh 10 40
+  DISABLE_SECCOMP=true VERBOSE=false ./multi-test-ce.sh 10 40
 done
